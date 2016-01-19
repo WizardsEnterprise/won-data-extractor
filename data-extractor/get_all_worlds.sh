8924884c4729a453059ea7eed0bdce7b8b0b5633
scornfield <corndog822@gmail.com>
2016-01-19 02:17:58 -0600

    Adding full path

--- data-extractor/get_all_worlds.sh
+++ data-extractor/get_all_worlds.sh
@@ -1,8 +1,8 @@
 for world in 1 2 3 4 5 6 7 8 9 10 11 12 14 15 16 17 18 19 20 21 22
 do
 	echo "World $world"
-	
-	php get_leaderboards.php $world > /var/log/won-data-extractor/get_leaderboards_"$world"_`date +\%Y\%m\%d\%H\%M`.log
-	php get_world.php $world > /var/log/won-data-extractor/get_world_"$world"_`date +\%Y\%m\%d\%H\%M`.log
-	php clean_ws_logs.php > /var/log/won-data-extractor/clean_ws_logs_`date +\%Y\%m\%d`.log
+
+	php /opt/won-data-extractor/data-extractor/get_leaderboards.php $world > /var/log/won-data-extractor/get_leaderboards_"$world"_`date +\%Y\%m\%d\%H\%M`.log
+	php /opt/won-data-extractor/data-extractor/get_world.php $world > /var/log/won-data-extractor/get_world_"$world"_`date +\%Y\%m\%d\%H\%M`.log
+	php /opt/won-data-extractor/data-extractor/clean_ws_logs.php > /var/log/won-data-extractor/clean_ws_logs_`date +\%Y\%m\%d`.log
 done
