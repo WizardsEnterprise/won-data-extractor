17574d5c2bb5c418d9fa4abec22bb20b0f46f018
scornfield <corndog822@gmail.com>
2016-01-24 15:14:00 -0600

    Rearranged scripts

--- data-extractor/clean_log_files.sh
+++
@@ -1,3 +0,0 @@
-cd /var/log/won-data-extractor/
-tar -cjvf archive/logs_`date +\%Y\%m\%d`.tar.bz *`date +\%Y\%m\%d`*.log  
-rm *`date +\%Y\%m\%d`*.log
---
+++ scripts/get_all_leaderboards.sh
@@ -0,0 +1,8 @@
+for world in 1 2 3 4 5 6 7 8 9 10 11 12 14 15 16 17 18 19 20 21 22
+do
+	echo "World $world"
+
+	php $WON_DIR/data-extractor/get_leaderboards.php $world > $WON_LOGS/get_leaderboards_"$world"_`date +\%Y\%m\%d\%H\%M`.log
+done
+
+php $WON_DIR/data-extractor/clean_ws_logs.php >> $WON_LOGS/clean_ws_logs_`date +\%Y\%m\%d`.log
