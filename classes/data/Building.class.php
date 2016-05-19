8ecda9c163140f8ce351b9223a720bd12f229b93
scornfield <corndog822@gmail.com>
2016-05-19 09:24:18 -0500

    Added building cache to reduce database calls

--- classes/data/Building.class.php
+++ classes/data/Building.class.php
@@ -1,11 +1,19 @@
 <?php
 class BuildingDAO {
+	// Cache these for performance during world map extractor
+	private static $building_cache = array();
+
 	public static function getAllBuildings($db) {
 		return $db->select("SELECT * FROM building_ref");
 	}
 	
 	public static function getLocalIdFromGameId($db, $game_building_id) {
-		return $db->selectValue("SELECT id FROM building_ref WHERE game_building_id=?", array($game_building_id));
+		// Try to find the building in cache
+		if(array_key_exists($game_building_id, self::$building_cache))
+			return $building_cache[$game_building_id];
+
+		// If it's not there... find it, save it, and return in
+		return $building_cache[$game_building_id] = $db->selectValue("SELECT id FROM building_ref WHERE game_building_id=?", array($game_building_id));
 	}
 }
 ?>
